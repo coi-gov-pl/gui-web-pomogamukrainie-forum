a87e12e03897598e9561cb611f44297f9706265c
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-04 16:17:03 +0100

    [POM-13] initial find help with categories

--- src/app/give-help/give-help.module.ts
+++ src/app/give-help/give-help.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GiveHelpComponent } from './give-help.component';
 import { GiveHelpRoutingModule } from './give-help-routing.module';
+<<<<<<< HEAD
 import { TranslateModule } from '@ngx-translate/core';
 import {AccommodationFormComponentModule} from "./accommodation-form/accommodation-form.module";
 
@@ -16,5 +17,13 @@ import {AccommodationFormComponentModule} from "./accommodation-form/accommodati
         AccommodationFormComponentModule,
       TranslateModule,
     ]
+=======
+import { MatCardModule } from '@angular/material/card';
+import { AccommodationFormComponentModule } from './accommodation-form/accommodation-form.module';
+
+@NgModule({
+  declarations: [GiveHelpComponent],
+  imports: [CommonModule, GiveHelpRoutingModule, MatCardModule, AccommodationFormComponentModule],
+>>>>>>> 8b6d656 ([POM-13] initial find help with categories)
 })
 export class GiveHelpModule {}
