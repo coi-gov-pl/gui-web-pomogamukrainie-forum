f6d95ed7270b64faf697104c1a43c24da174e8bb
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-04 16:35:30 +0100

    [POM-13] #PomagamUkrainie

--- src/app/give-help/give-help.module.ts
+++ src/app/give-help/give-help.module.ts
@@ -2,28 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GiveHelpComponent } from './give-help.component';
 import { GiveHelpRoutingModule } from './give-help-routing.module';
-<<<<<<< HEAD
-import { TranslateModule } from '@ngx-translate/core';
-import {AccommodationFormComponentModule} from "./accommodation-form/accommodation-form.module";
 
-@NgModule({
-  declarations: [
-    GiveHelpComponent
-  ],
-    imports: [
-        CommonModule,
-        GiveHelpRoutingModule,
-        MatCardModule,
-        AccommodationFormComponentModule,
-      TranslateModule,
-    ]
-=======
 import { MatCardModule } from '@angular/material/card';
 import { AccommodationFormComponentModule } from './accommodation-form/accommodation-form.module';
 
 @NgModule({
   declarations: [GiveHelpComponent],
   imports: [CommonModule, GiveHelpRoutingModule, MatCardModule, AccommodationFormComponentModule],
->>>>>>> 8b6d656 ([POM-13] initial find help with categories)
 })
 export class GiveHelpModule {}
