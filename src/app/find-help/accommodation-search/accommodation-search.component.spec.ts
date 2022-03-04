b359cdcb54617aee797a5ac32e28d05485204ac0
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-04 14:08:05 +0100

    [POM-13] accommodation search form

---
+++ src/app/find-help/accommodation-search/accommodation-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccommodationSearchComponent } from './accommodation-search.component';
+
+describe('AccommodationSearchComponent', () => {
+  let component: AccommodationSearchComponent;
+  let fixture: ComponentFixture<AccommodationSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AccommodationSearchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AccommodationSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/find-help/find-help.module.ts
+++ src/app/find-help/find-help.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FindHelpComponent } from './find-help.component';
-import { FindHelpRoutingModule } from './find-help-routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FindHelpComponent} from './find-help.component';
+import {FindHelpRoutingModule} from "./find-help-routing.module";
+import {AccommodationSearchComponentModule} from './accommodation-search/accommodation-search.component';
+import {MatCardModule} from "@angular/material/card";
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [FindHelpComponent],
-  imports: [CommonModule, FindHelpRoutingModule, TranslateModule],
+  declarations: [
+    FindHelpComponent
+  ],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    AccommodationSearchComponentModule,
+    TranslateModule,
+    FindHelpRoutingModule,
+  ]
 })
-export class FindHelpModule {}
+export class FindHelpModule { }
