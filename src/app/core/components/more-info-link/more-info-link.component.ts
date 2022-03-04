7e33fd24e57399c4ef5bee42d49af698ac73bb86
Bartosz Wojcicki <bartosz.wojcicki@snowflake.com>
2022-03-04 13:56:03 +0100

    Add more-info-link component

---
+++ src/app/core/components/more-info-link/more-info-link.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, NgModule } from "@angular/core";
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'more-info-link',
+  templateUrl: './more-info-link.component.html',
+  styleUrls: ['./more-info-link.component.scss']
+})
+export class MoreInfoLinkComponent {
+  @Input() href = '';
+}
+
+@NgModule({
+  declarations: [MoreInfoLinkComponent],
+  imports: [MatIconModule],
+  exports: [MoreInfoLinkComponent]
+})
+export class MoreInfoLinkModule { }
