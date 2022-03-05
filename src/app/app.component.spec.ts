4c46807a11641e414fb5f0eaf3d310a5eb309d6d
neu5 <neus90@gmail.com>
2022-03-05 09:05:12 +0100

    Fix 'pomogam' typo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pomogamukrainie'`, () => {
+  it(`should have as title 'pomagamukrainie'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pomogamukrainie');
+    expect(app.title).toEqual('pomagamukrainie');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pomogamukrainie app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pomagamukrainie app is running!');
   });
 });
