845e6b427fe1ca6f826fbf188e01ad000276a8c5
Artur Kozubek <artur.kozubek1@gmail.com>
2022-03-06 13:00:55 +0100

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,16 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { PomTranslateLoader, Language } from './core/translate-loader.service';
-import { CitiesSearchModule } from './cities-search/cities-search.module';
 import { AccommodationFormComponentModule } from './give-help/accommodation-form/accommodation-form.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CitiesSearchModule,
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatCardModule,
     AccommodationFormComponentModule,
-    CitiesSearchModule,
     TranslateModule.forRoot({
       defaultLanguage: Language.pl_PL,
       loader: {
