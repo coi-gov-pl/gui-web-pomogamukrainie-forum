c22951fabf23dc59007f64cbe037ae7018732b1c
Pawel Swiderski <pawel.swiderski@pentacomp.pl>
2022-03-14 14:15:32 +0100

    feat: POM-192 - [F] obsługa buttona dodaj ogłoszenie na ekranie moje konto

--- src/app/my-account/my-account/my-account.component.ts
+++ src/app/my-account/my-account/my-account.component.ts
@@ -3,6 +3,8 @@ import { OffersBaseOffer } from '@app/core/api';
 import { MyAccountPersonalData } from '../my-account.types';
 import { MyAccountService } from '../my-account.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { CorePath } from '@app/shared/models';
 
 @Component({
   selector: 'app-my-account',
@@ -12,9 +14,13 @@ import { Observable } from 'rxjs';
 export class MyAccountComponent implements OnInit {
   public myAccountPersonalData$: Observable<MyAccountPersonalData> | undefined;
   public myAnnouncements: Array<OffersBaseOffer> = [];
-  constructor(private myAccountService: MyAccountService) {}
+  constructor(private myAccountService: MyAccountService, private router: Router) {}
 
   public ngOnInit() {
     this.myAccountPersonalData$ = this.myAccountService.getPersonalData();
+  }
+
+  public addNewAd(): void {
+    this.router.navigate([CorePath.Give]);
   }
 }
