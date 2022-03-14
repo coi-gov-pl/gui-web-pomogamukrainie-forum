f194541441153f95f55e1a7163c4d824d9f1ce90
Pawel Swiderski <pawel.swiderski@pentacomp.pl>
2022-03-14 10:42:32 +0100

    feat: POM-141- Dodanie breadcrumbs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CorePath } from '@app/shared/models';
 import { MyAccountComponent } from './my-account/my-account/my-account.component';
+import { BreadcrumbLabels } from '@app/shared/models';
 
 const routes: Routes = [
   {
@@ -15,11 +16,17 @@ const routes: Routes = [
   {
     path: CorePath.Give,
     loadChildren: () => import('./give-help/give-help.module').then((m) => m.GiveHelpModule),
+    data: {
+      title: BreadcrumbLabels.ADD_AD,
+    },
   },
   {
     path: CorePath.MyAccount,
     component: MyAccountComponent,
     loadChildren: () => import('./my-account/my-account.module').then((m) => m.MyAccountModule),
+    data: {
+      title: BreadcrumbLabels.MY_ACCOUNT,
+    },
   },
 ];
 
--- src/app/give-help/give-help-routing.module.ts
+++ src/app/give-help/give-help-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GiveHelpComponent } from './give-help.component';
 import { CategoryRoutingName } from '@app/shared/models';
+import { BreadcrumbLabels } from '@app/shared/models';
 
 const routes: Routes = [
   {
@@ -12,15 +13,23 @@ const routes: Routes = [
         path: CategoryRoutingName.ACCOMMODATION,
         loadChildren: () =>
           import('./accommodation-form/accommodation-form.module').then((m) => m.AccommodationFormComponentModule),
+        data: {
+          title: BreadcrumbLabels.ACCOMMODATION,
+        },
       },
       {
         path: CategoryRoutingName.MATERIAL_HELP,
-        loadChildren: () => import('./material-aid-form/material-aid-form.module').then((m) => m.MaterialAidFormModule),
+        data: {
+          title: BreadcrumbLabels.MATERIAL_HELP,
+        },
       },
       {
         path: CategoryRoutingName.TRANSPORT,
         loadChildren: () =>
           import('./transport-form/transport-form.module').then((m) => m.TransportFormComponentModule),
+        data: {
+          title: BreadcrumbLabels.TRANSPORT,
+        },
       },
     ],
   },
