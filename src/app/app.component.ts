f31e1b25cc7246a62fe86ac53941c5c1108b68dd
jmeteor <jerzy.zientek@gmail.com>
2022-03-16 21:02:00 +0100

    Language switcher design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageCode } from '@app/core/translations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pomagamukrainie';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private translateService: TranslateService) {}
 
   getContentClass() {
     return this.router.url === '/' ? '' : 'header-padding';
+  }
+
+  ngOnInit() {
+    this.translateService.use(LanguageCode.pl_PL);
   }
 }
