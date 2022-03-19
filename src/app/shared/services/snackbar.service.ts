f94d34fa33dfd85f63e75a038705fb10f08eadcd
Piotr Klimaszewski <pklimski@gmail.com>
2022-03-19 14:08:11 +0100

    [POM-200] snackbar service update

--- src/app/shared/services/snackbar.service.ts
+++ src/app/shared/services/snackbar.service.ts
@@ -12,12 +12,12 @@ import { ALERT_TYPES } from '../models';
   providedIn: 'root',
 })
 export class SnackbarService {
-  private _horizontalPosition: MatSnackBarHorizontalPosition = 'center';
-  private _verticalPosition: MatSnackBarVerticalPosition = 'bottom';
-  private _actionButtonLabel = 'OK';
-  private _action = false;
-  private _setAutoHide = true;
-  private _autoHide = 5000;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  actionButtonLabel = 'OK';
+  action = false;
+  setAutoHide = true;
+  autoHide = 5000;
 
   constructor(private snackbar: MatSnackBar) {}
 
@@ -33,9 +33,9 @@ export class SnackbarService {
 
   openSnack(message: string, type: ALERT_TYPES) {
     const config = new MatSnackBarConfig();
-    config.verticalPosition = this._verticalPosition;
-    config.horizontalPosition = this._horizontalPosition;
-    config.duration = this._setAutoHide ? this._autoHide : 0;
+    config.verticalPosition = this.verticalPosition;
+    config.horizontalPosition = this.horizontalPosition;
+    config.duration = this.setAutoHide ? this.autoHide : 0;
     if (type === ALERT_TYPES.ERROR) {
       config.panelClass = ['snack-bar-error'];
     } else if (type === ALERT_TYPES.WARNING) {
@@ -43,30 +43,6 @@ export class SnackbarService {
     } else if (type === ALERT_TYPES.CONFIRM) {
       config.panelClass = ['snack-bar-confirm'];
     }
-    this.snackbar.open(message, this._action ? this._actionButtonLabel : undefined, config);
-  }
-
-  set horizontalPosition(value: MatSnackBarHorizontalPosition) {
-    this._horizontalPosition = value;
-  }
-
-  set verticalPosition(value: MatSnackBarVerticalPosition) {
-    this._verticalPosition = value;
-  }
-
-  set actionButtonLabel(value: string) {
-    this._actionButtonLabel = value;
-  }
-
-  set action(value: boolean) {
-    this._action = value;
-  }
-
-  set setAutoHide(value: boolean) {
-    this._setAutoHide = value;
-  }
-
-  set autoHide(value: number) {
-    this._autoHide = value;
+    this.snackbar.open(message, this.action ? this.actionButtonLabel : undefined, config);
   }
 }
