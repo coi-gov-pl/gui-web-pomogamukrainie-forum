e05ec27b85812d9f846e4b92d2bbc0c825bb5698
felix1313 <vradiuk13@gmail.com>
2022-03-22 10:34:07 +0100

    Add default query params to category navigation.

--- src/app/core/store-url/store-url.service.ts
+++ src/app/core/store-url/store-url.service.ts
@@ -41,14 +41,18 @@ export class StoreUrlService {
     return this.getPreviousUrl?.includes(routing) ? (params as Params) : null;
   }
 
+  getDefaultPaginatorQueryParams(): Params {
+    return {
+      page: 0,
+      size: localStorage.getItem(LocalStorageKeys.PageSize) ?? 5,
+      sort: [`modifiedDate,${SortingOrder.descending}`],
+    };
+  }
+
   async setDefaultPaginatorParam(): Promise<void> {
     await this.router.navigate([], {
       relativeTo: this.route,
-      queryParams: {
-        page: 0,
-        size: localStorage.getItem(LocalStorageKeys.PageSize) ?? 5,
-        sort: [`modifiedDate,${SortingOrder.descending}`],
-      },
+      queryParams: this.getDefaultPaginatorQueryParams(),
       queryParamsHandling: 'merge',
     });
   }
