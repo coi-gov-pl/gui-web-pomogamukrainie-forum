82432446d83c35153a78815452720c3db35dd51c
Piotr Klimaszewski <pklimski@gmail.com>
2022-03-23 15:18:36 +0100

    [POM-246][POM-235] location directive delete

--- src/app/shared/validators/validators-directives.module.ts
+++ src/app/shared/validators/validators-directives.module.ts
@@ -4,14 +4,12 @@ import { EmailValidateDirective } from './email-validate.directive';
 import { OfferDescriptionValidateDirective } from './offer-description-validate.directive';
 import { OfferTitleValidateDirective } from './offer-title-validate.directive';
 import { PhoneValidateValidateDirective } from './phone-validate.directive';
-import { LocationValidateDirective } from './location-validate.directive';
 
 const directives = [
   EmailValidateDirective,
   OfferDescriptionValidateDirective,
   OfferTitleValidateDirective,
   PhoneValidateValidateDirective,
-  LocationValidateDirective,
 ];
 
 @NgModule({
