de8c5a2357985e1046d8481e17bd46c1fada619f
Piotr Klimaszewski <pklimski@gmail.com>
2022-03-24 14:50:08 +0100

    [POM-292] update priv policies | missing RU

--- src/app/find-help/reply-offer/reply-offer.component.ts
+++ src/app/find-help/reply-offer/reply-offer.component.ts
@@ -2,10 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { defaults } from '@app/shared/utils';
 import { SendMessageDTO, MessageResourceService } from '@app/core/api';
 import { SnackbarService } from '@app/shared/services';
-import { ALERT_TYPES } from '@app/shared/models';
+import { ALERT_TYPES, LocalStorageKeys } from '@app/shared/models';
 import { switchMap, take } from 'rxjs';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 import { AuthService } from '@app/core/auth';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageCode } from '@app/core/translations';
 
 @Component({
   selector: 'app-reply-offer',
@@ -20,17 +22,20 @@ export class ReplyOfferComponent implements OnInit {
   @Input() helpersFirstname: string | undefined;
   showPhoneNumber: boolean = false;
   loading: boolean = false;
-
+  currentLang: LanguageCode = LanguageCode.pl_PL;
+  LanguageCode = LanguageCode;
   constructor(
     private messageResourceService: MessageResourceService,
     private snackbarService: SnackbarService,
     private reCaptchaV3Service: ReCaptchaV3Service,
-    public readonly authService: AuthService
+    public readonly authService: AuthService,
+    private translateService: TranslateService
   ) {}
 
   ngOnInit(): void {
-    this.data.tosApproved = false;
+    this.data.tosApproved = this.authService.isLoggedIn() ? true : false;
     this.data.offerId = this.offerId;
+    this.currentLang = this.translateService.store.currentLang as LanguageCode;
   }
 
   onConsentChange(): void {
