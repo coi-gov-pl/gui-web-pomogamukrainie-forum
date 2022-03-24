21198999b9beaf33fecd3e8ef137d8408feac8e3
Julian Kania <jkania@google.com>
2022-03-24 17:29:56 +0100

    Introduce language-contextual linking to privacy policy document.

--- src/app/core/site-footer/site-footer/site-footer.component.ts
+++ src/app/core/site-footer/site-footer/site-footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CorePath } from '@app/shared/models';
+import { LanguageCode } from '@app/core/translations';
+import { LocalStorageKeys } from '@app/shared/models';
 
 @Component({
   selector: 'app-site-footer',
@@ -9,4 +11,18 @@ import { CorePath } from '@app/shared/models';
 export class SiteFooterComponent {
   corePath = CorePath;
   constructor() {}
+
+  getSuffixForTranslatedResource() : string{
+    const code = localStorage.getItem(LocalStorageKeys.LangOption) || LanguageCode.pl_PL;
+    switch(code){
+      case LanguageCode.en_GB:
+        return '_EN';
+      case LanguageCode.uk_UA:
+        return '_UK';
+      case LanguageCode.ru_RU:
+        return '_RU';
+      default: // Polish translation handled as a spec-rule. Aligning: TODO in MVP2.
+        return '';
+    }
+  }
 }
