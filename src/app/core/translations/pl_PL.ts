2421105409382607551dba9bd4c4661107aafb58
Piotr Klimaszewski <pklimski@gmail.com>
2022-03-25 23:31:48 +0100

    [POM-254] max chars content edition (#239)

--- src/app/core/translations/pl_PL.ts
+++ src/app/core/translations/pl_PL.ts
@@ -140,7 +140,7 @@ export default {
   ERROR_MIN_DATE: 'Wprowadź późniejszą datę.',
   AND: 'i',
   PRIVATE_POLICY: 'politykę prywatności',
-  CHARS_LEFT: 'Pozostała ilość znaków:',
+  CHARS_LEFT: 'Pozostała liczba znaków:',
   NOT_FOUND: 'To ogłoszenie nie jest już dostępne.',
   NOT_FOUND_NEXT_ACTION: 'Wróć do listy ogłoszeń',
   NOT_FOUND_NEXT_ACTION_OR: 'lub',
