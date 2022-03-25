b3cb8cd8fa8b8b4de17ad80ba901deab8e21aed6
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2022-03-25 11:48:59 +0100

    POM-399 Use the NUMBER_OF_PEOPLE label in search results
    This removes inflection from the term which makes it easier to translate it.

--- src/app/shared/pipes/pluralize-polish/pluralize-polish.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PluralizePolishPipe } from './pluralize-polish.pipe';
-
-describe('PluralizePolishPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PluralizePolishPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/pipes/pom-common-pipes.module.ts
+++ src/app/shared/pipes/pom-common-pipes.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PluralizePolishPipe } from './pluralize-polish/pluralize-polish.pipe';
 import { PathPipe } from './path/path.pipe';
 
 @NgModule({
-  declarations: [PluralizePolishPipe, PathPipe],
-  exports: [PluralizePolishPipe, PathPipe],
+  declarations: [PathPipe],
+  exports: [PathPipe],
   imports: [CommonModule],
 })
 export class PomCommonPipesModule {}
