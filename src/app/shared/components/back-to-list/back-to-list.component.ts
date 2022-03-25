ea154f94ab14be21bfaddf143e79e2c7a03b6432
maciej <jan.przewoznik.2000@gmail.com>
2022-03-25 10:04:18 +0100

    https://github.com/coi-gov-pl/gui-web-pomogamukrainie-forum/pull/196/files

--- src/app/shared/components/back-to-list/back-to-list.component.ts
+++ src/app/shared/components/back-to-list/back-to-list.component.ts
@@ -11,6 +11,7 @@ import { CategoryRoutingName, CorePath } from '@app/shared/models';
 export class BackToListComponent implements OnChanges {
   @Input() categoryRouteName: CategoryRoutingName = CategoryRoutingName.ACCOMMODATION;
   @Input() redirectedFromAccount: boolean = false;
+  @Input() originalAccountQueryParams?: Params;
   constructor(private readonly router: Router, private readonly storeUrlService: StoreUrlService) {}
 
   backRoute: string[] = [];
@@ -20,7 +21,7 @@ export class BackToListComponent implements OnChanges {
     // if we are in context of "my account", navigate directly to /my-account instead of 'my-account/category'.
     if (this.redirectedFromAccount) {
       this.backRoute = ['/', CorePath.MyAccount];
-      this.queryParams = null;
+      this.queryParams = this.originalAccountQueryParams ?? null;
     } else {
       this.backRoute = [this.router.url.replace(/\/[^/]+$/, '')];
       this.queryParams = this.storeUrlService.getParams(this.categoryRouteName);
