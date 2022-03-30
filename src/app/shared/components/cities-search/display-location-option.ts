7e9cc5dd6781d437f968991df67b5f78784656b8
Jaroslaw Krol <jaroslawkrol11@gmail.com>
2022-03-30 21:18:14 +0200

    POM-433 Capitalize all words in cities' names

--- src/app/shared/components/cities-search/display-location-option.ts
+++ src/app/shared/components/cities-search/display-location-option.ts
@@ -20,8 +20,11 @@ export function displayLocationOption(location?: Location) {
   return toTitleCase(location.city) + ', ' + location.region;
 
   function toTitleCase(value: string) {
-    // DO NOT replace it with `value[0].toUpperCase()` etc. as that may not
-    // select the whole glyph, unlike unicode regex replacement.
-    return value.toLowerCase().replace(/^./u, (letter) => letter.toUpperCase());
+    // ^. - first character of the string
+    // \s. - first character after whitespace
+    // -. - first character after - (f.e. Kudowa-Zdroj)
+    return value
+      .toLowerCase()
+      .replace(/(^.|\s.|-.)([A-Za-zĄąĆćĘęŁłŃńÓóŚśŹźŻż]*)/g, (_, m1, m2) => m1.toUpperCase() + m2.toLowerCase());
   }
 }
