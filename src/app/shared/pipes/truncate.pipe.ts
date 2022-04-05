6908d43ed93578024db7e57a304fd00c24fa6c20
Piotr Klimaszewski <pklimski@gmail.com>
2022-04-05 09:51:25 +0200

    [POM-444] PR fixes

--- src/app/shared/pipes/truncate.pipe.ts
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -4,9 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate',
 })
 export class TruncatePipe implements PipeTransform {
-  transform(value: string, args: any[]): string {
-    const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
-    const trail = args.length > 1 ? args[1] : '...';
+  transform(value: string, limit = 20, trail = '...'): string {
     const limited = value.substring(0, limit);
     return value.length > limit ? limited.slice(0, limited.lastIndexOf(' ')) + trail : value;
   }
