955e53a1c4609acfeb049ba869475922eb76686b
Piotr Klimaszewski <pklimski@gmail.com>
2022-04-06 15:56:35 +0200

    [POM-210][POM-462] cancel button and dialog confirmation box

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,10 @@ import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { ValidatorsDirectivesModule } from './validators';
+import { ConfirmCancelDialogComponent } from './components/confirm-cancel-dialog/cancel-dialog.component';
 
 @NgModule({
-  declarations: [BreadcrumbComponent],
+  declarations: [BreadcrumbComponent, ConfirmCancelDialogComponent],
   imports: [CommonModule, RouterModule, MatIconModule, TranslateModule],
   exports: [MatCardModule, PomCommonPipesModule, BreadcrumbComponent, ValidatorsDirectivesModule],
 })
