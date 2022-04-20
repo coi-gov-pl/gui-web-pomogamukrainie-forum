73463f1f4d1cff31775594b35918b11eb8a04e35
Cezary C <101187771+cezmc@users.noreply.github.com>
2022-04-20 10:15:33 +0200

    Mock data fix. Replace Enum with strings. (#316)
    Co-authored-by: c.ciechanowski <c.ciechanowski@mc.gov.pl>

--- mock/data/health.ts
+++ mock/data/health.ts
@@ -6,7 +6,7 @@ export const healthOffer = (body: HealthOfferDefinitionDTO): HealthOffer => {
     id: 1,
     userFirstName: 'example',
     modifiedDate: new Date().toISOString(),
-    type: HealthOffer.TypeEnum.Health,
+    type: 'HEALTH',
   };
 };
 
