4ec42aa736383374edc31208ed25e72007d16c38
Piotr Klimaszewski <pklimski@gmail.com>
2022-04-22 00:09:01 +0200

    [POM-482] styles update - inline display only

--- src/app/shared/components/phone-input/phone-input.module.ts
+++ src/app/shared/components/phone-input/phone-input.module.ts
@@ -5,7 +5,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
 import { FieldErrorModule } from '@app/shared/components';
 import { ValidatorsDirectivesModule } from '@app/shared/validators';
 import { PhoneInputComponent } from './phone-input.component';
