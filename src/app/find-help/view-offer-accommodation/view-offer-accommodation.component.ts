01cef9de59144c2eb814a4c3da30fd908bdda938
Piotr Klimaszewski <pklimski@gmail.com>
2022-05-17 18:19:24 +0200

    [POM-169] offer slider - accommodation dirty no animation

--- src/app/find-help/view-offer-accommodation/view-offer-accommodation.component.ts
+++ src/app/find-help/view-offer-accommodation/view-offer-accommodation.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { AccommodationOffer, AccommodationsResourceService } from '@app/core/api';
+import { AccommodationOffer, AccommodationsResourceService, Pageable } from '@app/core/api';
 import { CategoryRoutingName, CorePath } from '@app/shared/models';
 import { defaults } from '@app/shared/utils';
+import { AccommodationQuery } from '../accommodation-search/accommodation-search-form/accommodation-search-form.component';
+import { OffersAccommodationOffer } from '@app/core/api';
 
 @Component({
   selector: 'app-view-offer-accommodation',
@@ -20,6 +22,13 @@ export class ViewOfferAccommodationComponent implements OnInit {
   color!: string;
   redirectedFromAccount: boolean;
   originalAccountQueryParams?: Params;
+  offerResults: AccommodationOffer[] = [];
+  activeOffer: AccommodationOffer | undefined;
+  nextOffer: AccommodationOffer | undefined;
+  accomodationAllResults: AccommodationOffer[] = [];
+  searchCriteria: AccommodationQuery = {};
+  total?: number = undefined;
+  activeIndex: number = 0;
   constructor(
     private route: ActivatedRoute,
     private accommodationsResourceService: AccommodationsResourceService,
@@ -32,12 +41,24 @@ export class ViewOfferAccommodationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+
     this.offerId = Number(this.route.snapshot.paramMap.get('id'));
-    this.getAccomodationOffer();
+    this.getAccomodationOffer(this.offerId);
+    // const { capacity, city, region } = this.route.snapshot.queryParams;
+    // const searchCriteria: AccommodationQuery = {
+    //   capacity,
+    //   location: {
+    //     region,
+    //     city,
+    //   },
+    // };
+    this.initAllResults();
+    console.log('accomodationAllResults', this.accomodationAllResults);
   }
 
-  getAccomodationOffer() {
-    this.accommodationsResourceService.getAccommodations(this.offerId).subscribe(
+  getAccomodationOffer(id: number) {
+    this.accommodationsResourceService.getAccommodations(id).subscribe(
       (response) => {
         this.data = response;
       },
@@ -45,5 +66,83 @@ export class ViewOfferAccommodationComponent implements OnInit {
         this.router.navigate([CorePath.Find, CategoryRoutingName.ACCOMMODATION, CategoryRoutingName.NOT_FOUND]);
       }
     );
+  }
+
+  getResultsObservable(
+    region: string | undefined,
+    city: string | undefined,
+    pageRequest: Pageable,
+    capacity: number | undefined
+  ) {
+    if (region && city) {
+      return this.accommodationsResourceService.listByLocationAccommodations(region, city, pageRequest, capacity);
+    } else {
+      return this.accommodationsResourceService.listAccommodations(pageRequest, capacity);
+    }
+  }
+
+  initAllResults() {
+    // const { page, size, sort } = this.route.snapshot.queryParams;
+    // if (searchCriteria) {
+    //   this.searchCriteria.capacity = searchCriteria?.capacity;
+    //   this.searchCriteria.location = searchCriteria?.location;
+    // }
+
+    const pageRequest: Pageable = {
+      page: undefined,
+      size: 999999999,
+      sort: undefined,
+    };
+
+    // const { location: { region, city } = {}, capacity } = this.searchCriteria;
+    this.accommodationsResourceService
+      .listAccommodations(pageRequest, undefined)
+      // this.getResultsObservable(undefined, undefined, pageRequest, undefined)
+      .subscribe({
+        next: (results) => {
+          this.offerResults = results.content ?? [];
+          this.total = results.totalElements;
+          console.log('this.offerResults', this.offerResults);
+          this.activeOffer = this.offerResults.find((x) => x.id === this.offerId);
+          this.activeIndex = this.offerResults.findIndex((x) => x.id === this.offerId);
+          console.log('this.activeOffer', this.activeOffer);
+          console.log('this.index', this.activeIndex);
+        },
+        error: () => {
+          this.offerResults = [];
+          this.total = undefined;
+        },
+      });
+    // this.activeOffer = this.offerResults.find((x) => x.id === this.offerId);
+
+    // this.offerResults.map((x) => x.id === this.offerId);
+  }
+
+  slideOffer(index: number, direction: 'prev' | 'next') {
+    console.log(index, direction);
+
+    if (direction === 'prev') {
+      // this.getAccomodationOffer(this.offerId);
+
+      const SLIDE_PREV_DATA: AccommodationOffer = this.offerResults[index - 1];
+      this.router.navigate([CorePath.Find, CategoryRoutingName.ACCOMMODATION, SLIDE_PREV_DATA.id]);
+      this.activeIndex = index >= 0 ? index - 1 : index;
+      this.offerId = this.activeIndex;
+      this.data = SLIDE_PREV_DATA;
+
+      // this.router.navigate([CorePath.Find, CategoryRoutingName.ACCOMMODATION, SLIDE_PREV_ID]).then((page) => {
+      //   window.location.reload();
+      // });
+    } else {
+      const SLIDE_NEXT_DATA: AccommodationOffer = this.offerResults[index + 1];
+      this.router.navigate([CorePath.Find, CategoryRoutingName.ACCOMMODATION, SLIDE_NEXT_DATA.id]);
+      this.activeIndex = index >= 0 ? index + 1 : index;
+      this.offerId = this.activeIndex;
+      this.data = SLIDE_NEXT_DATA;
+
+      // this.router.navigate([CorePath.Find, CategoryRoutingName.ACCOMMODATION, SLIDE_NEXT_ID]).then((page) => {
+      //   window.location.reload();
+      // });
+    }
   }
 }
