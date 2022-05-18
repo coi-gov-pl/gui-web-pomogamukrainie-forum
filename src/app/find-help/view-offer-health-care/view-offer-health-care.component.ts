722a57e6c8c2fc942c97a4da1b4564f2d9b41b60
Piotr Klimaszewski <pklimski@gmail.com>
2022-05-18 19:21:44 +0200

    [POM-169] slider for health offers

--- src/app/find-help/view-offer-health-care/view-offer-health-care.component.ts
+++ src/app/find-help/view-offer-health-care/view-offer-health-care.component.ts
@@ -5,6 +5,7 @@ import { defaults } from '@app/shared/utils';
 import { CorePath } from '@app/shared/models';
 import { HealthOffer } from '../../core/api/model/healthOffer';
 import { HealthResourceService } from '../../core/api/api/healthResource.service';
+import { HealthOfferSearchCriteria, Pageable } from '@app/core/api';
 
 @Component({
   selector: 'app-view-offer-health-care',
@@ -16,7 +17,13 @@ export class ViewOfferHealthCareComponent implements OnInit {
   data = defaults<HealthOffer>();
   categoryRouteName = CategoryRoutingName.HEALTH;
   redirectedFromAccount: boolean;
-  originalAccountQueryParams?: Params;
+  originalAccountQueryParams?: Params | undefined;
+  offerResults: HealthOffer[] = [];
+  activeOffer: HealthOffer | undefined;
+  activeIndex: number = 0;
+  blurClass = '';
+  searchCriteria = defaults<HealthOfferSearchCriteria>();
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -42,5 +49,48 @@ export class ViewOfferHealthCareComponent implements OnInit {
         this.router.navigate([CorePath.Find, CategoryRoutingName.HEALTH, CategoryRoutingName.NOT_FOUND]);
       }
     );
+  }
+
+  getResults() {
+    this.searchCriteria.location = this.originalAccountQueryParams?.['location'];
+    this.searchCriteria.specialization = this.originalAccountQueryParams?.['specialization'];
+    this.searchCriteria.language = this.originalAccountQueryParams?.['language'];
+    this.searchCriteria.mode = this.originalAccountQueryParams?.['mode'];
+
+    const PAGEREQUEST: Pageable = {
+      page: undefined,
+      size: 999999999,
+      sort: undefined,
+    };
+
+    return this.healthResourceService.listHealth(PAGEREQUEST, this.searchCriteria).subscribe((results) => {
+      this.offerResults = results.content ?? [];
+      this.activeOffer = this.offerResults.find((x) => x.id === this.offerId);
+      this.activeIndex = this.offerResults.findIndex((x) => x.id === this.offerId);
+    });
+  }
+
+  slideOffer(index: number, direction: 'prev' | 'next') {
+    this.blurAnimate();
+    if (direction === 'prev') {
+      const SLIDE_PREV_DATA: HealthOffer = this.offerResults[index - 1];
+      this.router.navigate([CorePath.Find, CategoryRoutingName.HEALTH, SLIDE_PREV_DATA.id]);
+      this.activeIndex = index >= 0 ? index - 1 : index;
+      this.offerId = this.activeIndex;
+      this.data = SLIDE_PREV_DATA;
+    } else {
+      const SLIDE_NEXT_DATA: HealthOffer = this.offerResults[index + 1];
+      this.router.navigate([CorePath.Find, CategoryRoutingName.HEALTH, SLIDE_NEXT_DATA.id]);
+      this.activeIndex = index >= 0 ? index + 1 : index;
+      this.offerId = this.activeIndex;
+      this.data = SLIDE_NEXT_DATA;
+    }
+  }
+
+  blurAnimate() {
+    this.blurClass = 'blurClass';
+    setTimeout(() => {
+      this.blurClass = '';
+    }, 300);
   }
 }
