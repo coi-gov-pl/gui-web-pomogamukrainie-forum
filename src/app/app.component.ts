7380cd5440abae7d9f5a910ede25b321b0e37416
c.ciechanowski <c.ciechanowski@mc.gov.pl>
2022-05-19 09:32:03 +0200

    POM-589 Track url change by Matomo script - missing import OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageCode } from '@app/core/translations';
@@ -38,7 +38,7 @@ async function addJiraScript() {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   private destroyed$: Subject<void> = new Subject<void>();
 
   constructor(
