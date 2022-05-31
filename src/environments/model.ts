2a8be7702b3bdfb0bae20ca6eb5acedb6af06344
Piotr Klimaszewski <pklimski@gmail.com>
2022-05-31 19:55:05 +0200

    [POM-417] display dialog when session expired

--- src/environments/model.ts
+++ src/environments/model.ts
@@ -3,9 +3,8 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export interface EnvironmentConfig {
   production: boolean;
   environmentType: EnvironmentType;
-  authConfig: Required<Pick<AuthConfig, 'showDebugInformation' | 'redirectUri' | 'logoutUrl'>>;
-  //   authConfig: Required<Pick<AuthConfig, 'issuer' | 'showDebugInformation' | 'redirectUri' | 'logoutUrl'>>;
-  // applicationInsightsConnectionString?: string;
+  authConfig: Required<Pick<AuthConfig, 'issuer' | 'showDebugInformation' | 'redirectUri' | 'logoutUrl'>>;
+  applicationInsightsConnectionString?: string;
   recaptcha: {
     siteKey: string;
   };
